2073c16754bd0e1871ef93178e31fe92dde80ebf
Plagne <splagne@gmail.com>
2021-11-16 22:41:08 +0100

    picker category et software

--- src/screens/CategoryScreen.jsx
+++ src/screens/CategoryScreen.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Button, ScrollView } from "react-native";
 import {Picker} from '@react-native-picker/picker';
+import ShortcutScreen from "./ShortcutScreen";
 
 
 
 export default function CategoryScreen(props) {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
-  // const [shortcuts, setShortcuts] = useState([]);
+  const [shortcuts, setShortcuts] = useState([]);
+
   
   useEffect(() => {
     fetch(process.env.API_URL + "categories")
@@ -18,8 +20,33 @@ export default function CategoryScreen(props) {
 
   const categorieJsx = categories
     .sort((c1, c2) => c1.name.localeCompare(c2.name))
-    .map((c) => <Button key={c.id} label={c.name} value={c.id} />);
+    .map((c) => <Picker.Item key={c.id} label={c.name} value={c.id} />);
 
+
+  const shortcutsJsx = shortcuts.map((s) => (
+    <View key={s.id}>
+      <Text>
+        {s.title}
+      </Text>
+      
+    </View>
+  ))  
+
+
+
+
+  // const productsJsx = Details.products.map((p) => (
+  //   <View key={p['@id']} style={styles.productContainer}>
+  //       <Image style={styles.image} source={{uri: getImageUrl(p.image)}} />
+  //       <View style={styles.productDetails}>
+  //           <Text style={styles.productTitle}>{p.name}</Text>
+  //           <Text>{p.price} euros</Text>
+
+  //       </View>
+      
+
+  //   </View>
+  // )
   
   return (
     <ScrollView>
@@ -33,11 +60,13 @@ export default function CategoryScreen(props) {
               .then((data) => setShortcuts(data["hydra:member"]))
               .catch((error) => console.log(error));
           setSelectedCategory(c);
-        }}
-      >
+        }}>
+
           {categorieJsx}
+
       </Picker>
 
+          {shortcutsJsx}
 
     </ScrollView>
   )
--- src/screens/SoftwareScreen.jsx
+++ src/screens/SoftwareScreen.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Button, ScrollView, Text, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
+
+
 export default function SoftwareScreen(props) {
   const [software, setSoftware] = useState([]);
   const [selectedSoftware, setSelectedSoftware] = useState();
+  const [shortcuts, setShortcuts] = useState ([]);
 
   useEffect(() => {
     fetch(process.env.API_URL + "software")
@@ -15,23 +18,36 @@ export default function SoftwareScreen(props) {
 
   const softwareJsx = software
     .sort((s1, s2) => s1.name.localeCompare(s2.name))
-    .map((s) => <Button key={s.id} label={s.name} value={s.id} />);
+    .map((s) => <Picker.Item key={s.id} label={s.name} value={s.id} />);
+
+  const shortcutsJsx = shortcuts.map((s) => (
+    <View key={s.id}>
+      <Text>
+        {s.title}
+      </Text>
+    </View>
+  ))  
 
   return (
     <ScrollView>
       <Picker
         selectedValue={selectedSoftware}
         style={{ backgroundColor: "#ffbe9f", margin: 15 }}
-        onValueChange={function (s) {
-          fetch(process.env.API_URL + "shortcuts?software.id=" + s)
-            .then((response) => response.json())
-            .then((data) => setShortcuts(data["hydra:member"]))
-            .catch((error) => console.log(error));
-          setSelectedSoftware(s);
-        }}
-      >
-        {softwareJsx}
+        onValueChange={
+          function (s) {
+            fetch(process.env.API_URL + "shortcuts?software.id=" + s)
+              .then((response) => response.json())
+              .then((data) => setShortcuts(data["hydra:member"]))
+              .catch((error) => console.log(error));
+            setSelectedSoftware(s);
+        }}>
+        
+          {softwareJsx}
+
       </Picker>
+
+          {shortcutsJsx}
+
     </ScrollView>
-  );
+  )
 }
